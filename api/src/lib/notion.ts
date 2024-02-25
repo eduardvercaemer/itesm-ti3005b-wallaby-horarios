b6689a89d30bb4b3498c2d4be10323138ff4b1de
ed <vercaemereduard@gmail.com>
2024-02-24 23:19:13 -0600

    basic zod parser for classes database

--- api/src/lib/notion.ts
+++ api/src/lib/notion.ts
@@ -1,11 +1,13 @@
 import { Core } from "../core.ts";
 import { getClassDatabaseId } from "./database.ts";
 import { datePlus } from "itty-time";
+import { z } from "zod";
+import { NotionError } from "../errors/notion-error.ts";
 
 export async function queryClassForRange(c: Core, date: Date) {
   const databaseId = await getClassDatabaseId(c);
 
-  return await c.notion.databases.query({
+  const response = await c.notion.databases.query({
     database_id: databaseId,
     filter: {
       and: [
@@ -22,4 +24,48 @@ export async function queryClassForRange(c: Core, date: Date) {
       ],
     },
   });
+
+  console.debug(JSON.stringify(response, null, 2));
+
+  const parsed = await z
+    .object({
+      results: z.array(
+        z
+          .object({
+            id: z.string().uuid(),
+            url: z.string().url(),
+            properties: z.object({
+              Date: z.object({
+                type: z.literal("date"),
+                date: z.object({
+                  start: z.string().transform((s) => new Date(s)),
+                  end: z.string().transform((s) => new Date(s)),
+                }),
+              }),
+              Employee: z.object({
+                type: z.literal("people"),
+                people: z.array(
+                  z.object({
+                    object: z.literal("user"),
+                    id: z.string().uuid(),
+                  }),
+                ),
+              }),
+            }),
+          })
+          .transform((o) => ({
+            id: o.id,
+            url: o.url,
+            date: o.properties.Date.date,
+            employee: o.properties.Employee.people.map((p) => p.id),
+          })),
+      ),
+    })
+    .transform((response) => response.results)
+    .safeParseAsync(response);
+  if (parsed.success) {
+    return parsed.data;
+  }
+
+  throw new NotionError(parsed.error.message, { cause: parsed.error });
 }
